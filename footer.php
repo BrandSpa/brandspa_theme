58e51ee209fe1a33bfa29660adad3748996cf32d
Michael Sanchez <user@Users-MacBook-Pro-2.local>
2018-11-08 13:50:33 -0500

    add ready

--- footer.php
+++ footer.php
@@ -54,9 +54,9 @@ if (window.addEventListener) {
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.plugins.min.js"></script>
 <script>
-$(function() {
+  $(document).ready(function(){
     $('.project-item').lazy();
-});
+  })
 </script>
 
 </body>

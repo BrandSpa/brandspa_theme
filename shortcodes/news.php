078a58b2fcf6c8b84d0090154672ca2da1fb13f3
Michael Sanchez <user@Users-MacBook-Pro-2.local>
2018-11-08 13:27:20 -0500

    change background

--- shortcodes/news.php
+++ shortcodes/news.php
@@ -117,7 +117,7 @@ function ra_news_sc( $atts ) {
         <a href="<?php echo esc_url( get_permalink($post->ID) ); ?>" class="project-item">
         <div
 			class="project-item__header"
-			style="backgroundImage: `url(<?php echo $post->post_image ?>)"
+			style="background: url(<?php echo $post->post_image ?>); background-size: 100%;"
 		></div>
 
 		<div class="project__info">

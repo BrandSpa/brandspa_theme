7f9f884d82923dc3200d0b30dc7644f9f0b64315
Michael <michaelsanchez@brandspa.com>
2019-08-15 10:21:36 -0500

    add menu.js

--- util/menu.js
+++ util/menu.js
@@ -1,12 +1,15 @@
-$(document).ready(function(){
-	const header = $(".fade-transparent");
-	$(window).scroll(function() {
-		var scroll = $(window).scrollTop();
-		if (scroll >= 200) {
-			header.removeClass('fade-transparent').addClass("fade-background");
-		} else {
-			header.removeClass("fade-background").addClass('fade-transparent');
+(function($){
+	$(document).ready(function(){
+		const header = $(".fade-transparent");
+		$(window).scroll(function() {
+			var scroll = $(window).scrollTop();
+			if (scroll >= 200) {
+				header.removeClass('fade-transparent').addClass("fade-background");
+			} else {
+				header.removeClass("fade-background").addClass('fade-transparent');
 
-		}
-	});
-})
+			}
+		});
+	})
+
+})(jQuery)
